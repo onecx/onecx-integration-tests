39bd1a5360ba66fa36757d1da8905affc029abd2
mholzaepfel <204693613+mholzaepfel@users.noreply.github.com>
2025-11-25 10:06:24 +0100

    chore: remove .github workflows chore: remove angular specific settings chore: remove dockerfile and the corresponding ignore file

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-const nx = require('@nx/eslint-plugin');
+const nx = require('@nx/eslint-plugin')
 
 module.exports = [
   ...nx.configs['flat/base'],
@@ -12,32 +12,4 @@ module.exports = [
     // Override or add rules here
     rules: {},
   },
-  ...nx.configs['flat/angular'],
-  ...nx.configs['flat/angular-template'],
-  {
-    files: ['**/*.ts'],
-    rules: {
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          type: 'attribute',
-          prefix: 'app',
-          style: 'camelCase',
-        },
-      ],
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          type: 'element',
-          prefix: 'app',
-          style: 'kebab-case',
-        },
-      ],
-    },
-  },
-  {
-    files: ['**/*.html'],
-    // Override or add rules here
-    rules: {},
-  },
-];
+]
